c5fa29f1e59f479a3bda89df18336625403e7dbf
Paulo Martin <paumar559@gmail.com>
2021-02-02 17:47:48 -0800

    Fixed Local Storage

--- JS/admin.js
+++ JS/admin.js
@@ -1,17 +1,22 @@
 "use strict"
 
-var admin = prompt("Hello, please identify yourself");
+/// Fetchs Catalog Infromation
+    function fetchCatalogData(categoryName) { // categoryName is the List Name in local storage
+        var reObjectify = localStorage.getItem(categoryName);
+        var productsFromStorage = JSON.parse(reObjectify);
+        return productsFromStorage;
+      }
 
-console.log(admin)
+// while ( admin !== "admin" ) {
+// var admin = prompt("Hello, please identify yourself");
+// }
 
-var password = prompt("Hello," + admin + " please enter your password")
-"admin" = admin
-"password" = password
-var button = document.getElementById("button");
+// while ( password !== "password"){
+// var password = prompt("Hello," + admin + " please enter your password")
+// }
 
-button.addEventListener("click",function(event) {
-  console.log(event.target)
-})
+
+fetchCatalogData("Nurse Members List")
 
 
 
@@ -25,15 +30,135 @@ function SearchKit(nurseName, titles, email, cityOfOrg, certsJobsSkills, current
   this.currentOrg = currentOrg
   }
 
-///// Fetchs Catalog Infromation
-//     function fetchCatalogData(categoryName) { // categoryName is the List Name in local storage
-//         var reObjectify = localStorage.getItem(categoryName);
-//         var productsFromStorage = JSON.parse(reObjectify);
-//         return productsFromStorage;
-//       }
+var button = document.getElementById("button");
 
-///// Stores Catalog Information
-//     function storeCatalogData(categoryName, object) { // categoryName is List Name in local storage and object is an object
-//         var stringObject = JSON.stringify(object);
-//         localStorage.setItem(categoryName, stringObject);
-//       }
+///// Initializer
+var unwrapKit = fetchCatalogData("Nurse Members List")
+console.log(unwrapKit.item[0].nurseName)
+
+var nameArray = []
+var titlesArray = []
+var emailArray = []
+var cityOfOrg = []
+var certsJobsSkillsArray = []
+var currentOrgArray = []
+
+
+
+for (var i =0; i < unwrapKit.item.length; i++) {
+    nameArray.push(unwrapKit.item[i].nurseName)
+    titlesArray.push(unwrapKit.item[i].titles)
+    emailArray.push(unwrapKit.item[i].email)
+    cityOfOrg.push(unwrapKit.item[i].cityOfOrg)
+    certsJobsSkillsArray.push(unwrapKit.item[i].certsJobsSkills)
+    currentOrgArray.push(unwrapKit.item[i].currentOrg)
+}
+
+var globalTest = new SearchKit(nameArray, titlesArray, emailArray, cityOfOrg, certsJobsSkillsArray, currentOrgArray)
+console.log(globalTest)
+table_builder()
+tableData(globalTest)
+
+
+
+
+
+///// Button Click Event
+button.addEventListener("click",function(event) {
+event.preventDefault()
+
+
+
+  var unwrapKit = fetchCatalogData("Nurse Memebers List")
+  console.log(unwrapKit.item[0].nurseName)
+
+  var nameArray = []
+  var titlesArray = []
+  var emailArray = []
+  var cityOfOrg = []
+  var certsJobsSkillsArray = []
+  var currentOrgArray = []
+
+
+
+  for (var i =0; i < unwrapKit.item.length; i++) {
+      nameArray.push(unwrapKit.item[i].nurseName)
+      titlesArray.push(unwrapKit.item[i].titles)
+      emailArray.push(unwrapKit.item[i].email)
+      cityOfOrg.push(unwrapKit.item[i].cityOfOrg)
+      certsJobsSkillsArray.push(unwrapKit.item[i].certsJobsSkills)
+      currentOrgArray.push(unwrapKit.item[i].currentOrg)
+  }
+
+  var test = new SearchKit(nameArray, titlesArray, emailArray, cityOfOrg, certsJobsSkillsArray, currentOrgArray)
+  console.log(test)
+  refresh_table()
+  table_builder()
+  tableData(test)
+
+
+
+
+})
+
+///// Table Refresh
+function refresh_table(){
+  var tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = ""
+}
+
+///// Builds Table function
+function table_builder() {
+  
+  var tableBody = document.getElementById('table-body');
+  var tableRow = document.createElement('tr')
+  var headingInfo = ["Name", "Title", "Email", "City of Origin", "Certifications & Job Skills",  "Current Organization"]
+  
+
+  for (var i= 0; i < headingInfo.length;  i++){
+  var heading = document.createElement('th')
+  heading.textContent = headingInfo[i]
+  tableRow.appendChild(heading)
+  }
+  tableBody.appendChild(tableRow);
+
+}
+
+
+function tableData(searchKit) {
+
+      for (var i=0; i < searchKit.nurseName.length; i++) {
+        var tableBody = document.getElementById('table-body');
+        var tableRow = document.createElement('tr')
+
+            var tableData = document.createElement('td')
+            tableData.textContent = searchKit.nurseName[i]
+            tableRow.appendChild(tableData)
+            var tableData = document.createElement('td')
+            tableData.textContent = searchKit.titles[i]
+            tableRow.appendChild(tableData)
+            var tableData = document.createElement('td')
+            tableData.textContent = searchKit.email[i]
+            tableRow.appendChild(tableData)
+            var tableData = document.createElement('td')
+            tableData.textContent = searchKit.cityOfOrg[i]
+            tableRow.appendChild(tableData)
+            var tableData = document.createElement('td')
+            tableData.textContent = searchKit.certsJobsSkills[i]
+            tableRow.appendChild(tableData)
+            var tableData = document.createElement('td')
+            tableData.textContent = searchKit.currentOrg[i]
+            tableRow.appendChild(tableData)
+
+            tableBody.appendChild(tableRow);
+      }
+}
+
+
+
+
+/// Stores Catalog Information
+    function storeCatalogData(categoryName, object) { // categoryName is List Name in local storage and object is an object
+        var stringObject = JSON.stringify(object);
+        localStorage.setItem(categoryName, stringObject);
+      }
--- JS/app.js
+++ JS/app.js
@@ -3,18 +3,16 @@
 var formElement = document.getElementById('registration-form')
 
 ///// FancyKat stores your objects for you
-var FancyKat = function(item) {
+var FancyKat = function(item) { // Connects to Prototype
     this.item = item;
   }; // We only call FancyKat for commands
 
 ///// Stores one Item into FancyKat to store later
-var fancyNurse = new FancyKat([])
-
 
 ///// Stores in FancyKat for local storage
-FancyKat.prototype.storeRegistration = function(nurseName, titles, email, cityOfOrg, certsJobsSkills, currentOrg) {
-
-    this.item.push(new Nurse(nurseName, titles, email, cityOfOrg, certsJobsSkills, currentOrg))
+FancyKat.prototype.storeRegistration = function(object) {
+    
+    this.item.push(object)
 };
 
 
@@ -29,41 +27,33 @@ function Nurse(nurseName, titles, email, cityOfOrg, certsJobsSkills, currentOrg)
     }
 
 
+
+
+
+
+
 /// Stores Catalog Information
 function storeCatalogData(categoryName, object) { // categoryName is List Name in local storage and object is an object
     var stringObject = JSON.stringify(object);
     localStorage.setItem(categoryName, stringObject);
     }
 
+
+
+
+
+
 ///// Fetchs Catalog Infromation
 function fetchCatalogData(categoryName) { // categoryName is the List Name in local storage
     var reObjectify = localStorage.getItem(categoryName);
+    if (reObjectify === null) {
+        return null
+    }
     var productsFromStorage = JSON.parse(reObjectify);
     return productsFromStorage;
 }
 
-///// Event Listner to take information from the submmission
-formElement.addEventListener('submit', function (event) {
-    event.preventDefault();
 
-    var nurseName = event.target.nurseName.value
-    var titles = event.target.titles.value
-    var email = event.target.email.value
-    var cityOfOrg = event.target.cityOfOrg.value
-    var certsJobsSkills = event.target.certsJobsSkills.value
-    var currentOrg = event.target.currentOrg.value
-
-    //// Pushed is to FancyKat Storage
-    fancyNurse.storeRegistration(nurseName, titles, email, cityOfOrg, certsJobsSkills, currentOrg)
-
-    storeCatalogData("Nurse Memebers List", fancyNurse)
-    if (fancyNurse.item.length <= 1){
-    table_builder()
-    }
-
-    tableData()
-    console.log(fancyNurse)
-})
 
 ///// Builds Table function
 function table_builder() {
@@ -108,4 +98,51 @@ function tableData() {
     console.log(fancyNurse.item)
 }
 
+var fancyNurse = new FancyKat([]) 
+
+console.log(fetchCatalogData("Nurse Members List") !== null)
+if (fetchCatalogData("Nurse Members List") !== null){
+    var Nurse_member = fetchCatalogData("Nurse Members List")
+        console.log(Nurse_member, "From Storage")
+    for (var i= 0; i < Nurse_member.item.length; i++){
+        var testNurse = Nurse_member.item[i]
+        fancyNurse.storeRegistration(testNurse)
+    }
+
+}
+
+// console.log(fetchCatalogData("Nurse Members List"))
+// console.log(FancyKat)
+
+
+
+///// Event Listner to take information from the submmission
+formElement.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    var nurseName = event.target.nurseName.value
+    var titles = event.target.titles.value
+    var email = event.target.email.value
+    var cityOfOrg = event.target.cityOfOrg.value
+    var certsJobsSkills = event.target.certsJobsSkills.value
+    var currentOrg = event.target.currentOrg.value
+
+
+    
+
+    var nurse = new Nurse(nurseName, titles, email, cityOfOrg, certsJobsSkills, currentOrg)
+
+    console.log(nurse)
+    //// Pushed is to FancyKat Storage
+    fancyNurse.storeRegistration(nurse)
+
+    storeCatalogData("Nurse Members List", fancyNurse)
+    if (fancyNurse.item.length <= 1){
+    table_builder()
+    }
+
+    tableData()
+    console.log(fancyNurse)
+})
 console.log("KICK ASSS")
